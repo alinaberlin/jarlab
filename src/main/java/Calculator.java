fc6af320fc0043783b2c9e38b57186944d2e1423
alinaberlin <alina.ghetler@gmail.com>
2023-10-02 21:25:58 +0200

    first commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+
+    public int  add(int a, int b){
+        return  a+ b;
+    }
+    public int  minus(int a, int b){
+        return  a- b;
+    }
+    public int multiplicate(int a, int b){
+        return a*b;
+    }
+
+}
---
+++ src/main/java/MainApp.java
@@ -0,0 +1,9 @@
+public class MainApp {
+
+    public static void main(String[] args){
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(1, 2));
+        System.out.println(calculator.minus(1, 2));
+        System.out.println(calculator.multiplicate(1, 2));
+    }
+}
